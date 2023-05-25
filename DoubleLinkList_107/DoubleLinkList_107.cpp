88ad17c6d8a125ef95f84e43011e7156106aa0d4
rezaikram <muhammadikramriza@gmail.com>
2023-05-25 20:48:01 +0700

    langkah 1

--- DoubleLinkList_107/DoubleLinkList_107.cpp
+++ DoubleLinkList_107/DoubleLinkList_107.cpp
@@ -28,3 +28,12 @@ public:
 DoubleLinkedList::DoubleLinkedList() {
 	START = NULL;
 }
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+	Node* newNode = new Node(); //step 1
