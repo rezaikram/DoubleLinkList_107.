10507f0fa6e4028a9ccc9c326c8b88436aaceaf5
rezaikram <muhammadikramriza@gmail.com>
2023-05-25 20:48:44 +0700

    langkah 2

--- DoubleLinkList_107/DoubleLinkList_107.cpp
+++ DoubleLinkList_107/DoubleLinkList_107.cpp
@@ -37,3 +37,5 @@ void DoubleLinkedList::addNode() {
 	cout << "\nEnter the name of the student: ";
 	cin >> nm;
 	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim; // step 2
+	newNode->name = nm; // step 2
