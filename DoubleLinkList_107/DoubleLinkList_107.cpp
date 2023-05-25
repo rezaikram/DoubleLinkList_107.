1998ac53d9374fdf1da50afef33dae5934c1cb85
rezaikram <muhammadikramriza@gmail.com>
2023-05-25 20:52:37 +0700

    memasukkan node

--- DoubleLinkList_107/DoubleLinkList_107.cpp
+++ DoubleLinkList_107/DoubleLinkList_107.cpp
@@ -53,3 +53,23 @@ void DoubleLinkedList::addNode() {
 		START = newNode; // step 6
 		return;
 	}
+	/*Inserting a node between two nodes in the list*/
+	Node* current = START; // step 1.a
+	Node* previous = NULL; // step 1.
+	while (current->next != NULL && current->next->noMhs < nim) // step 1,c
+	{
+		previous = current; // 1,d
+		current = current->next; // 1.e
+	}
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
+
+	newNode->next = current->next; // step 4
+	newNode->prev = current; // step 5
+	if (current->next != NULL)
+		current->next->prev = newNode; // step 6
+	current->next = newNode; // step 7
+}
